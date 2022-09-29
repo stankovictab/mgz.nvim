16fd8caebc2300e816619a09a8ce84dbda1700ec
stankovictab <stankovictab@gmail.com>
2022-09-29 19:21:43 +0200

    Black & White

--- lua/mgz/named_colors.lua
+++ lua/mgz/named_colors.lua
@@ -2,14 +2,14 @@
 
 local mgz = {
 	--16 colors
-	black = "#2E3440", -- mgz0 in palette
+	black = "#000", -- mgz0 in palette
 	dark_gray = "#3B4252", -- mgz1 in palette
 	gray = "#434C5E", -- mgz2 in palette
 	light_gray = "#4C566A", -- mgz3 in palette
 	light_gray_bright = "#616E88", -- out of palette
 	darkest_white = "#D8DEE9", -- mgz4 in palette
 	darker_white = "#E5E9F0", -- mgz5 in palette
-	white = "#ECEFF4", -- mgz6 in palette
+	white = "#fff", -- mgz6 in palette
 	teal = "#8FBCBB", -- mgz7 in palette
 	off_blue = "#88C0D0", -- mgz8 in palette
 	glacier = "#81A1C1", -- mgz9 in palette
