aaf6d76b3eb71c5942bc4bb3ab914e10e71d583f
stankovictab <stankovictab@gmail.com>
2022-09-29 19:28:17 +0200

    None black, comment, string

--- lua/mgz/named_colors.lua
+++ lua/mgz/named_colors.lua
@@ -1,8 +1,8 @@
 -- The first file, table of base 16 colors
 
 local mgz = {
-	--16 colors
-	black = "#000", -- mgz0 in palette
+	-- black = "#000", -- mgz0 in palette
+	black = "NONE", -- mgz0 in palette
 	dark_gray = "#3B4252", -- mgz1 in palette
 	gray = "#434C5E", -- mgz2 in palette
 	light_gray = "#4C566A", -- mgz3 in palette
@@ -12,12 +12,12 @@ local mgz = {
 	white = "#fff", -- mgz6 in palette
 	teal = "#8FBCBB", -- mgz7 in palette
 	off_blue = "#88C0D0", -- mgz8 in palette
-	glacier = "#81A1C1", -- mgz9 in palette
+	glacier = "#658595", -- mgz9 in palette
 	blue = "#5E81AC", -- mgz10 in palette
 	red = "#BF616A", -- mgz11 in palette
 	orange = "#D08770", -- mgz12 in palette
 	yellow = "#EBCB8B", -- mgz13 in palette
-	green = "#A3BE8C", -- mgz14 in palette
+	green = "#58F5AB", -- mgz14 in palette
 	purple = "#B48EAD", -- mgz15 in palette
 	none = "NONE",
 }
