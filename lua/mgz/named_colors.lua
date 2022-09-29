682d665539dd20cce053a73430cfcc2ea62e01a7
stankovictab <stankovictab@gmail.com>
2022-09-29 19:36:55 +0200

    Random colors

--- lua/mgz/named_colors.lua
+++ lua/mgz/named_colors.lua
@@ -2,22 +2,22 @@
 
 local mgz = {
 	-- black = "#000", -- mgz0 in palette
-	black = "NONE", -- mgz0 in palette
-	dark_gray = "#3B4252", -- mgz1 in palette
-	gray = "#434C5E", -- mgz2 in palette
-	light_gray = "#4C566A", -- mgz3 in palette
-	light_gray_bright = "#616E88", -- out of palette
-	darkest_white = "#D8DEE9", -- mgz4 in palette
-	darker_white = "#E5E9F0", -- mgz5 in palette
+	black = "NONE", -- mgz0 in palette, background
+	dark_gray = "#fB4252", -- mgz1 in palette
+	gray = "#4acC5E", -- mgz2 in palette
+	light_gray = "#4C56fA", -- mgz3 in palette
+	light_gray_bright = "#ff0000", -- out of palette
+	darkest_white = "#ff9E88", -- mgz4 in palette
+	darker_white = "#05f9ff", -- mgz5 in palette
 	white = "#fff", -- mgz6 in palette
 	teal = "#8FBCBB", -- mgz7 in palette
 	off_blue = "#88C0D0", -- mgz8 in palette
-	glacier = "#658595", -- mgz9 in palette
+	glacier = "#658595", -- mgz9 in palette, comment
 	blue = "#5E81AC", -- mgz10 in palette
 	red = "#BF616A", -- mgz11 in palette
 	orange = "#D08770", -- mgz12 in palette
 	yellow = "#EBCB8B", -- mgz13 in palette
-	green = "#58F5AB", -- mgz14 in palette
+	green = "#58F5AB", -- mgz14 in palette, string
 	purple = "#B48EAD", -- mgz15 in palette
 	none = "NONE",
 }
