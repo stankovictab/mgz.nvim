9c9fae7b76a918adf365af5ec236ba2c2a68da4b
stankovictab <stankovictab@gmail.com>
2022-10-14 16:00:58 +0200

    Visual fix

--- lua/mgz/theme.lua
+++ lua/mgz/theme.lua
@@ -156,7 +156,7 @@ theme.loadEditor = function()
 		TablineSel = { fg = mgz.col_dimblack, bg = mgz.col_white },
 		Tabline = { fg = mgz.col_blue, bg = mgz.col_dimblack },
 		Title = { fg = mgz.col_teal, bg = mgz.none, style = "bold" },
-		Visual = { fg = mgz.none, bg = mgz.col_dimred }, -- ?
+		Visual = { fg = mgz.none, bg = mgz.col_darkgray }, -- Color of highlight in Visual Mode
 		VisualNOS = { fg = mgz.none, bg = mgz.col_darkgray },
 		WarningMsg = { fg = mgz.col_black }, -- Overrides FIXME and XXX fg for some reason
 		WildMenu = { fg = mgz.col_green, bg = mgz.none, style = "bold" },
