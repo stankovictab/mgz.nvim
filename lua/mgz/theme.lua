d99460f6985e1a2dfe5563493082064b7be52fde
Đorđe Stanković <stankovictab@gmail.com>
2023-06-08 05:54:58 +0200

    BufferLine Colors 2

--- lua/mgz/theme.lua
+++ lua/mgz/theme.lua
@@ -409,12 +409,13 @@ theme.loadPlugins = function() -- Plugins highlight groups
 		-- LuaLine is in another file
 
 		-- BufferLine
-		BufferLineBufferSelected = { fg = mgz.col_white, bg = mgz.col_fours, gui = "bold", style = "bold", italic = "false" }, -- Selected buffer text and bg
+		BufferLineBufferSelected = { fg = mgz.col_white, bg = mgz.col_dimwhite, gui = "bold", style = "bold", italic = "false" }, -- Selected buffer text and bg
 		BufferLineBackground = { fg = mgz.col_gray, bg = mgz.col_diminbetween }, -- Unselected buffer text and bg
 		BufferLineModified = { fg = mgz.col_yellow, bg = mgz.none, gui = "bold", style = "bold" }, -- Indicator (dot) that the buffer has been modified
 		BufferLineModifiedSelected = { fg = mgz.col_yellow, bg = mgz.none, gui = "bold", style = "bold" }, -- Same, but for selected buffer
-		BufferLineFill = { fg = mgz.none, bg = mgz.col_dimblack }, -- BufferLine background, for the BufferLine itself, not counting the tabs
+		BufferLineFill = { fg = mgz.none, bg = mgz.col_darkgray }, -- BufferLine background, for the BufferLine itself, not counting the tabs
 		BufferLineSeparator = { fg = mgz.col_dimblack, bg = mgz.col_dimblack }, -- Separator between tabs, fg is the tiny line, bg is the background
+		BufferLineIndicatorSelected = { fg = mgz.col_dimblack, bg = mgz.col_dimblack }, -- The indicator (leftmost) for the selected buffer
 
 		-- NvimScrollbar -- For some reason it can't be set here, so it's set in the lua config file of the plugin
 
